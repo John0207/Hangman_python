05d7741dd3186709737b1ae0ab54ae602e22cfdd
John Deluccia <john.deluccia@snhu.edu>
2020-09-30 16:53:30 -0400

    lowers case and strips to first letter of input, random word chosen from word list

--- hangman.py
+++ hangman.py
@@ -2,6 +2,7 @@
 This is a hangman game from the book
 The self-taught programmer by Cory Althoff
 '''
+from random import randint
 
 
 def hangman(word):
@@ -19,16 +20,20 @@ def hangman(word):
     board = ["_"] * len(word)
     win = False
     print("Welcome To Hangman")
+    print(word)
 
     while wrong < len(stages) - 1:
         print("\n")
-        msg = "Guess a letter "
-        char = input(msg)
+        msg = "Guess one letter at a time: "
+        char = input(msg).lower().strip()
+        char = char[0]
         if char in rletters:
+            print(char)
             cind = rletters.index(char)
             board[cind] = char
             rletters[cind] = '$'
         else:
+            print(char)
             wrong += 1
         print((" ".join(board)))
         e = wrong + 1
@@ -40,7 +45,9 @@ def hangman(word):
             break
     if not win:
         print("\n".join(stages[0: wrong]))
-        print("You lose! It was { }.".format(word))
+        print("You lose! It was " + word)
 
 
-hangman("cat")
+word_list = ["apples", "bananas", "strawberries", "kiwis", "pears", "pineapples"]
+word = word_list[randint(0, (word_list.__len__())) - 1]
+hangman(word)
